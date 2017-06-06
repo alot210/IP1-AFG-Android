cfbb572c0640645717afcbb94f9d9926d4026b90
Thomas <schloemer.thomas@gmail.com>
2017-06-06 22:22:24 +0200

    Seekbar implemented

--- app/src/main/java/de/hsd/manguli/fractalsapp/EditorActivityFragment.java
+++ app/src/main/java/de/hsd/manguli/fractalsapp/EditorActivityFragment.java
@@ -6,17 +6,86 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * Fragment als Teil des Editor
  */
 public class EditorActivityFragment extends Fragment {
 
+    //Seekbar Iterationen
+    SeekBar sb_iter;
+    //Seekbar Geschwindigkeit Animation
+    SeekBar sb_speed;
+
+    //Textfeld Iterationen
+    TextView tv_iter;
+    //Textfeld Geschwindigkeit
+    TextView tv_speed;
+
     public EditorActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_editor, container, false);
+        //View erstellen, um darüber die XML Elemente ansprechen zu können
+        View editor_m = inflater.inflate(R.layout.fragment_editor, container, false);
+
+        //Layout Elemente über ID ansprechen
+        //Iteration
+        sb_iter=(SeekBar) editor_m.findViewById(R.id.seekBar_m_Iteration);
+        sb_iter.setProgress(0);
+        sb_iter.incrementProgressBy(10);
+        sb_iter.setMax(100);
+
+        sb_speed=(SeekBar) editor_m.findViewById(R.id.seekBar_m_speed);
+
+        tv_iter=(TextView) editor_m.findViewById(R.id.text_m_Iteration_value);
+        tv_speed=(TextView) editor_m.findViewById(R.id.text_m_Speed_value);
+
+        //Wird bei Veränderung der Seekbar aufgerufen
+        sb_iter.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            //Wenn Wert verändert wird
+            @Override
+            public void onProgressChanged(SeekBar sb_iter, int progress, boolean fromUser) {
+
+                progress = progress / 10;
+                progress = progress * 10;
+                //in Textfeld den Wert schreiben
+                tv_iter.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar sb_iter) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar sb_iter) {
+
+            }
+        });
+
+        sb_speed.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar sb_speed, int progress, boolean fromUser) {
+                tv_speed.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar sb_speed) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar sb_speed) {
+
+            }
+        });
+
+        return editor_m;
     }
 }
--- app/src/main/java/de/hsd/manguli/fractalsapp/Julia_Fragment.java
+++ app/src/main/java/de/hsd/manguli/fractalsapp/Julia_Fragment.java
@@ -6,18 +6,85 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class Julia_Fragment extends Fragment {
 
+    //Seekbar Iterationen
+    SeekBar sb_iter;
+    //Seekbar Geschwindigkeit Animation
+    SeekBar sb_speed;
+
+    //Textfeld Iterationen
+    TextView tv_iter;
+    //Textfeld Geschwindigkeit
+    TextView tv_speed;
+
     public Julia_Fragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_julia, container, false);
+        //View erstellen, um darüber die XML Elemente ansprechen zu können
+        View editor_j = inflater.inflate(R.layout.fragment_julia, container, false);
+
+        //Layout Elemente über ID ansprechen
+        //Iteration
+        sb_iter = (SeekBar) editor_j.findViewById(R.id.seekBar_j_Iteration);
+        sb_iter.setProgress(0);
+        sb_iter.incrementProgressBy(10);
+        sb_iter.setMax(100);
+
+        sb_speed = (SeekBar) editor_j.findViewById(R.id.seekBar_j_speed);
+
+        tv_iter = (TextView) editor_j.findViewById(R.id.text_j_Iteration_value);
+        tv_speed = (TextView) editor_j.findViewById(R.id.text_j_Speed_value);
+
+        //Wird bei Veränderung der Seekbar aufgerufen
+        sb_iter.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            //Wenn Wert verändert wird
+            @Override
+            public void onProgressChanged(SeekBar sb_iter, int progress, boolean fromUser) {
+
+                progress = progress / 10;
+                progress = progress * 10;
+                //in Textfeld den Wert schreiben
+                tv_iter.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar sb_iter) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar sb_iter) {
+
+            }
+        });
+
+        sb_speed.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar sb_speed, int progress, boolean fromUser) {
+                tv_speed.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar sb_speed) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar sb_speed) {
+
+            }
+        });
+
+        return editor_j;
     }
 }
 
