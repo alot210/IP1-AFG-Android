f022425a63807d3235a1b541e49da21d88f90408
Jana Borchardt <borchardt.jana@web.de>
2017-06-17 22:08:00 +0200

    Multi-Threading implementiert: Mandelbrot wird gemalt, aber noch nicht in Logik des aktuellen Release eingebunden

--- app/src/main/java/de/hsd/manguli/fractalsapp/FractalView.java
+++ app/src/main/java/de/hsd/manguli/fractalsapp/FractalView.java
@@ -10,7 +10,9 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Picture;
 import android.graphics.PorterDuff;
+import android.text.format.DateUtils;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 import com.flask.colorpicker.ColorPickerView;
@@ -22,7 +24,10 @@ import de.hsd.manguli.fractalsapp.Complex;
 
 import java.lang.reflect.Array;
 import java.text.AttributedCharacterIterator;
+import java.util.ArrayList;
+import java.util.List;
 
+import static android.content.ContentValues.TAG;
 
 
 /**
@@ -34,6 +39,10 @@ public class FractalView extends View {
     Canvas bitmapCanvas = null;
     EditorActivityFragment eaf = new EditorActivityFragment();
     Julia_Fragment jf = new Julia_Fragment();
+
+    private List<Thread> currentThreads = new ArrayList<>();
+    private volatile boolean terminateThreads;
+    private static Boolean onCall = false;
 
     //Überschreiben der drei Constructor
     public FractalView(Context context) {
@@ -59,6 +68,8 @@ public class FractalView extends View {
         point = new Paint();
         point.setColor(0xff101010);
 
+        //Default-Menge beim Start
+
     }
 
     /**
@@ -66,90 +77,125 @@ public class FractalView extends View {
      * Hier wird angegeben, was in der View gezeichnet wird
      * @param canvas das übergebene Canvas Objekt, mit dem gezeichnet wird
      */
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        //Existiert ein Bitmap?
-        if (bitmap == null) {
-            //Bitmap erzeugen mit den Canvas Maßen
-            bitmap = Bitmap.createBitmap(canvas.getWidth(), canvas.getHeight(), Bitmap.Config.ARGB_8888);
-            //Canvas mit Bitmap erzeugen
-            bitmapCanvas = new Canvas(bitmap);
-            bitmapCanvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+            //Existiert ein Bitmap?
+            //damit onDraw-Methode nur einmal ausgeführt wird (ansonsten wird sie zweimal mit onCreate und onResume aufgerufen)
+            //if(onCall) {
+            if (bitmap == null) {
+                //Bitmap erzeugen mit den Canvas Maßen
+                bitmap = Bitmap.createBitmap(canvas.getWidth(), canvas.getHeight(), Bitmap.Config.ARGB_8888);
+                //Canvas mit Bitmap erzeugen
+                bitmapCanvas = new Canvas(bitmap);
+                bitmapCanvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+            }
+            //Methode zum Mandelbrot zeichnen aufrufen und in Canvas speichern
+            canvas.drawColor(Color.WHITE, PorterDuff.Mode.CLEAR);
+            //Bitmap auf Canvas ausgeben
+            drawFractal(bitmapCanvas);
+            canvas.drawBitmap(bitmap, 0, 0, point);
+            System.out.println("onDraw");
+            /*onCall = false;
+            return;
+
         }
-        //Methode zum Mandelbrot zeichnen aufrufen und in Canvas speichern
-        drawOnCanvas(bitmapCanvas);
-        //Bitmap auf Canvas ausgeben
-        canvas.drawBitmap(bitmap, 0, 0, point);
+        onCall = true;*/
+        }
 
-    }
+        public void drawFractal(Canvas canvas){
+            System.out.println("drawFractal");
+            Mandelbrot mb = new Mandelbrot(canvas.getWidth(),canvas.getHeight(), 20 ,new Complex(2.0,2.0),new Complex(3.0,4.0));
+            terminateThreads();
+            Thread t, t1, t2, t3, t4;
 
-    public void drawOnCanvas(Canvas canvas){
-        canvas.drawColor(Color.WHITE, PorterDuff.Mode.CLEAR);
-        //Mandelbrot Objekt erstellen
+        /*t1 = new MandelThread(mb, canvas, 0, canvas.getWidth(), canvas.getHeight()/4, Color.RED);
+        t2 = new MandelThread(mb, canvas, canvas.getHeight()/4, canvas.getWidth(), canvas.getHeight()/2, Color.BLUE);
+        t3 = new MandelThread(mb, canvas, canvas.getHeight()/2, canvas.getWidth(), (canvas.getHeight()*3)/4, Color.GREEN);
+        t4 = new MandelThread(mb, canvas, (canvas.getHeight()*3)/4, canvas.getWidth(), canvas.getHeight(),Color.YELLOW);
 
-        Boolean juliaPush = Julia_Fragment.juliaPush;
-        Boolean mandelPush = EditorActivityFragment.mandelPush;
-        int mbItm = Integer.parseInt(EditorActivityFragment.iterartion);
+        t2.start();
+        t1.start();
+        t3.start();
+        t4.start();
+        currentThreads.add(t1);
+        currentThreads.add(t2);
+        currentThreads.add(t3);
+        currentThreads.add(t4);*/
 
-        if(!juliaPush & !mandelPush){
-            Mandelbrot mb = new Mandelbrot(canvas.getWidth(),canvas.getHeight(),mbItm,new Complex(2.0,2.0),new Complex(3.0,4.0));
+            int numberOfThreads = 8;
+            int startY = 0, stopY = canvas.getHeight()/numberOfThreads, startX = canvas.getWidth();
+            for(int i = 0; i < numberOfThreads; i++){
 
-            //An den Punkten in der View zeichnen
-            for (int i=0; i< canvas.getWidth();i++){
-                for(int j=0; j< canvas.getHeight(); j++){
-                    //Farbe und Style setzen
-                    point.setColor(mb.setColor(i,j));
-                    //point.setColor(mb.setColor(i,j));
-                    point.setStyle(Paint.Style.FILL);
-                    //Punkt zeichnen
-                    canvas.drawPoint(i,j,point);
-                }
+                //Thread initialisieren
+                t = new MandelThread(mb, canvas, startY, startX, stopY);
+                //Run-Methode starten
+                t.start();
+                //Threads zur Liste hinzufügen
+                currentThreads.add(t);
+                //Nächster Abschnitt vom Mandelbrot
+                startY = stopY;
+                stopY += canvas.getHeight()/numberOfThreads;
             }
         }
-        else if(mandelPush & !juliaPush) {
-            int color1 = EditorActivityFragment.color1;
-            int color2 = EditorActivityFragment.color2;
-            int color3 = EditorActivityFragment.color3;
-            int color4 = EditorActivityFragment.color4;
-            Mandelbrot mb = new Mandelbrot(canvas.getWidth(),canvas.getHeight(),mbItm,new Complex(2.0,2.0),new Complex(3.0,4.0));
 
-            //An den Punkten in der View zeichnen
-            for (int i=0; i< canvas.getWidth();i++){
-                for(int j=0; j< canvas.getHeight(); j++){
-                    //Farbe und Style setzen
-                    point.setColor(mb.setColor(i,j,color1,color2/*,color3,color4*/));
-                    point.setStyle(Paint.Style.FILL);
-                    //Punkt zeichnen
-                    canvas.drawPoint(i,j,point);
+        private class MandelThread extends Thread
+        {
+
+            private int startY, stopX, stopY;
+            private Canvas canvas;
+            private Paint paint = new Paint();
+            private Algorithm m;
+
+            MandelThread (Algorithm m, Canvas canvas, int startY, int stopX, int stopY) {
+                this.canvas = canvas;
+                this.startY = startY;
+                this.stopX = stopX;
+                this.stopY = stopY;
+                this.m = m;
+            }
+
+            @Override
+            public void run(){
+
+                //canvas.drawRect(0, startY , stopX, stopY, point);
+
+                //An den Punkten in der View zeichnen
+                for (int i=0; i< stopX;i++){
+                    for(int j=startY; j< stopY; j++){
+                        //Punkt zeichnen
+                        paint.setColor(m.setColor(i, j));
+                        paint.setStyle(Paint.Style.FILL);
+                        canvas.drawPoint(i,j,paint);
+                        //System.out.println("[ID"+this.getId()+"]"+i+", "+j);
+                    }
                 }
+
             }
         }
-        else {
-            int jItm = Integer.parseInt(Julia_Fragment.iterartion);
-            double _real = Math.cos((double)Julia_Fragment.real+3.257);
-            double _imag = Math.sin((double)Julia_Fragment.imag);
-            int color1 = Julia_Fragment.color1;
-            int color2 = Julia_Fragment.color2;
-            int color3 = Julia_Fragment.color3;
-            int color4 = Julia_Fragment.color4;
-            Julia ju = new Julia(canvas.getWidth(),canvas.getHeight(),jItm,new Complex(1.5,2.0),new Complex(3.0,4.0),new Complex(-0.7,-0.3));
+        public void terminateThreads() {
+            try {
+                // mandelnative.SignalTerminate(paramIndex);
+                terminateThreads = true;
 
-            //An den Punkten in der View zeichnen
-            for (int i=0; i< canvas.getWidth();i++){
-                for(int j=0; j< canvas.getHeight(); j++){
-                    //Farbe und Style setzen
-                    point.setColor(ju.setColor(i,j,color1,color2/*,color3,color4*/));
-                    point.setStyle(Paint.Style.FILL);
-                    //Punkt zeichnen
-                    canvas.drawPoint(i,j,point);
+                for(Thread t : currentThreads){
+                    if (t.isAlive()) {
+                        t.join(DateUtils.SECOND_IN_MILLIS);
+                        System.out.println("thread terminated");
+                    }
+                    if (t.isAlive()) {
+                        Log.w(TAG, "Thread is still alive after 1sec...");
+                    }
                 }
+
+                terminateThreads = false;
+                currentThreads.clear();
+            } catch(Exception ex) {
+                Log.w(TAG, "Exception while terminating threads: " + ex.getMessage());
             }
-            Julia_Fragment.juliaPush = false;
         }
-
-
-    }//end drawOnCanvas()
 
     //Methode gibt ein Teil des Color Picker Dialogfensters zurück
     public ColorPickerDialogBuilder getColorPickerDialogBuilder() {
