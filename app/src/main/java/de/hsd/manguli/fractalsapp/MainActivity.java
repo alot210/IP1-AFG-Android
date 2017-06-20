450c32114d716b7a76d02efe7d51af9097e4413a
Thomas <schloemer.thomas@gmail.com>
2017-06-20 15:54:17 +0200

    Erste Implementierung, fehlerhaft

--- app/src/main/java/de/hsd/manguli/fractalsapp/MainActivity.java
+++ app/src/main/java/de/hsd/manguli/fractalsapp/MainActivity.java
@@ -1,27 +1,41 @@
 package de.hsd.manguli.fractalsapp;
 
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Attr;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * MainActivity - wird beim Start der App aufgerufen
  * hier befindet sich die View --> Ausgabe der Mengen
  */
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +52,9 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setLogo(R.drawable.hsd_logo_weiss);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
-
+        //View initialisieren und mit invalidate() onDraw aufrufen
+        final FractalView fw = new FractalView(this);
+        fw.invalidate();
         //Screenshot Button als FAB (=Floating Action Button) integrieren
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         //Snackbar Meldung bei Klick auf FAB
@@ -47,12 +63,13 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Hier wird im finalen Release ein Screenshot gespeichert.", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                //store(getScreenShot(fw), "demo");
+                temp(fw);
+                //shareImage();
             }
         });
 
-        //View initialisieren und mit invalidate() onDraw aufrufen
-        FractalView fw = new FractalView(this);
-        fw.invalidate();
+
     }
 
     @Override
@@ -92,4 +109,75 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+/*
+    public static Bitmap getScreenShot(View view) {
+        View screenView = view.getRootView();
+        screenView.setDrawingCacheEnabled(true);
+        Bitmap bitmap = Bitmap.createBitmap(screenView.getDrawingCache());
+        screenView.setDrawingCacheEnabled(false);
+        return bitmap;
+    }
+
+    public static void store(Bitmap bm, String fileName){
+        final String dirPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Screenshots";
+        File dir = new File(dirPath);
+        if(!dir.exists())
+            dir.mkdirs();
+        File file = new File(dirPath, fileName);
+        try {
+            FileOutputStream fOut = new FileOutputStream(file);
+            bm.compress(Bitmap.CompressFormat.PNG, 85, fOut);
+            fOut.flush();
+            fOut.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private void shareImage(File file){
+        Uri uri = Uri.fromFile(file);
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.setType("image/*");
+
+        intent.putExtra(android.content.Intent.EXTRA_SUBJECT, "");
+        intent.putExtra(android.content.Intent.EXTRA_TEXT, "");
+        intent.putExtra(Intent.EXTRA_STREAM, uri);
+        try {
+            startActivity(Intent.createChooser(intent, "Share Screenshot"));
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(this, "No App Available", Toast.LENGTH_SHORT).show();
+        }
+    }*/
+    private void temp(View v){
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss a");
+        String currentDateandTime = sdf.format(new Date());
+
+        Bitmap bitmap = Bitmap.createBitmap(v.getMeasuredWidth(),
+                v.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
+        Canvas bitmapHolder = new Canvas(bitmap);
+        v.draw(bitmapHolder);
+        try {
+            final File f = savebitmap(bitmap,currentDateandTime);
+            Log.e("File Loc",f.toString());
+            Toast.makeText(this, "Image saved to "+f.toString()+" successfully", Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static File savebitmap(Bitmap bmp, String currentDateandTime) throws IOException {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 60, bytes);
+        File f = new File(Environment.getExternalStorageDirectory()
+                + File.separator + "MobeeLoad/"+currentDateandTime+".jpg");
+        f.createNewFile();
+        FileOutputStream fo = new FileOutputStream(f);
+        fo.write(bytes.toByteArray());
+        fo.close();
+        bmp.recycle();
+        return f;
+    }
+
+
 }
