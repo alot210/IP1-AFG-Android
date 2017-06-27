e6b163515974e8d56f2c2e654a9f9143b8abcf20
Thomas <schloemer.thomas@gmail.com>
2017-06-27 16:15:22 +0200

    Screenshott Library included

--- app/src/main/java/de/hsd/manguli/fractalsapp/MainActivity.java
+++ app/src/main/java/de/hsd/manguli/fractalsapp/MainActivity.java
@@ -2,6 +2,7 @@ package de.hsd.manguli.fractalsapp;
 
 import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
@@ -9,6 +10,7 @@ import android.graphics.Canvas;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -30,6 +32,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import github.nisrulz.screenshott.ScreenShott;
 
 /**
  * MainActivity - wird beim Start der App aufgerufen
@@ -63,9 +67,10 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Hier wird im finalen Release ein Screenshot gespeichert.", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-                //store(getScreenShot(fw), "demo");
-                temp(fw);
-                //shareImage();
+
+                // RootView
+                Bitmap bitmap_rootview = ScreenShott.getInstance().takeScreenShotOfRootView(view);
+                ScreenShott.getInstance().saveScreenshotToPicturesFolder(MainActivity.this, bitmap_rootview, "my_screenshot_filename");
             }
         });
 
@@ -105,79 +110,7 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
 
-
-
         return super.onOptionsItemSelected(item);
     }
-/*
-    public static Bitmap getScreenShot(View view) {
-        View screenView = view.getRootView();
-        screenView.setDrawingCacheEnabled(true);
-        Bitmap bitmap = Bitmap.createBitmap(screenView.getDrawingCache());
-        screenView.setDrawingCacheEnabled(false);
-        return bitmap;
-    }
-
-    public static void store(Bitmap bm, String fileName){
-        final String dirPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Screenshots";
-        File dir = new File(dirPath);
-        if(!dir.exists())
-            dir.mkdirs();
-        File file = new File(dirPath, fileName);
-        try {
-            FileOutputStream fOut = new FileOutputStream(file);
-            bm.compress(Bitmap.CompressFormat.PNG, 85, fOut);
-            fOut.flush();
-            fOut.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    private void shareImage(File file){
-        Uri uri = Uri.fromFile(file);
-        Intent intent = new Intent();
-        intent.setAction(Intent.ACTION_SEND);
-        intent.setType("image/*");
-
-        intent.putExtra(android.content.Intent.EXTRA_SUBJECT, "");
-        intent.putExtra(android.content.Intent.EXTRA_TEXT, "");
-        intent.putExtra(Intent.EXTRA_STREAM, uri);
-        try {
-            startActivity(Intent.createChooser(intent, "Share Screenshot"));
-        } catch (ActivityNotFoundException e) {
-            Toast.makeText(this, "No App Available", Toast.LENGTH_SHORT).show();
-        }
-    }*/
-    private void temp(View v){
-        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss a");
-        String currentDateandTime = sdf.format(new Date());
-
-        Bitmap bitmap = Bitmap.createBitmap(v.getMeasuredWidth(),
-                v.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
-        Canvas bitmapHolder = new Canvas(bitmap);
-        v.draw(bitmapHolder);
-        try {
-            final File f = savebitmap(bitmap,currentDateandTime);
-            Log.e("File Loc",f.toString());
-            Toast.makeText(this, "Image saved to "+f.toString()+" successfully", Toast.LENGTH_LONG).show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    public static File savebitmap(Bitmap bmp, String currentDateandTime) throws IOException {
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        bmp.compress(Bitmap.CompressFormat.JPEG, 60, bytes);
-        File f = new File(Environment.getExternalStorageDirectory()
-                + File.separator + "MobeeLoad/"+currentDateandTime+".jpg");
-        f.createNewFile();
-        FileOutputStream fo = new FileOutputStream(f);
-        fo.write(bytes.toByteArray());
-        fo.close();
-        bmp.recycle();
-        return f;
-    }
-
 
 }
