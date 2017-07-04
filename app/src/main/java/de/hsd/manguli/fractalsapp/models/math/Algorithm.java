ba9940464c7f9a02ab3b8dc86f46f2d595523021
Jana Borchardt <borchardt.jana@web.de>
2017-07-04 16:46:11 +0200

    Comments, Java Doc added, Alorithm generates Pixel-Array now

--- app/src/main/java/de/hsd/manguli/fractalsapp/models/math/Algorithm.java
+++ app/src/main/java/de/hsd/manguli/fractalsapp/models/math/Algorithm.java
@@ -12,23 +12,19 @@ public class Algorithm {
     public Algorithm() {
     }
 
-    ;
     //Fenstergröße
     private int width;
-    private int heigth;
-
+    private int height;
     //Default-Werte
     private int maxIteration = 150;
     private int color1 = Color.YELLOW, color2 = Color.CYAN, color3 = Color.MAGENTA, color4 = Color.BLUE;
-    //Array mit interpolierten Farben
-    public int[] palette = new int[maxIteration];
 
     public int getWidth() {
         return this.width;
     }
 
     public int getHeigth() {
-        return this.heigth;
+        return this.height;
     }
 
     public int getMaxIteration() {
@@ -40,12 +36,13 @@ public class Algorithm {
     }
 
     public void setHeigth(int _height) {
-        this.heigth = _height;
+        this.height = _height;
     }
 
     public void setMaxIteration(int _m) {
         this.maxIteration = _m;
     }
+
     public int getColor1() {
         return color1;
     }
@@ -80,18 +77,21 @@ public class Algorithm {
 
     //Mittelpunkt des Bildausschnitts berechnen:
     //spaeter beim Verschieben werden diese Werte dynamisch geändert
-
     //Die Mandelbrot-Menge wird im Fenster bei w/2 und (3/4)*h zentriert
     Complex translate;
     Complex scale;
 
-    //Methode wird nur von Julia-Menge überschrieben
-    //Methode überprüft für jede übergebene Komplexe Zahl Zugehörigkeit zur Menge
-    public int isElemOfMand(Complex z0, int max){
+    /**
+     * Methode wird nur von Julia-Menge überschrieben
+     * Methode überprüft für jede übergebene Komplexe Zahl Zugehörigkeit zur Menge
+     * @param z0 Konstante aus Folge
+     * @return Farbe für Pixel
+     */
+    public int isElemOfMand(Complex z0){
 
         Complex z = z0;
 
-        for (int n = 0; n < max; n++) {   //n Zahl der Iterationen
+        for (int n = 0; n < maxIteration; n++) {   //n Zahl der Iterationen
 
             if (z.pythagoras() > 4.0) {  //|Zn| > 2    => |Zn| = sqrt(x^2+y^2), dann ist Zahl C ausserhalb der Menge
                 //setColor(n)
@@ -111,14 +111,19 @@ public class Algorithm {
         return color1;  //|Zn| < 2, Farbe ist schwarz
     }
 
-    //Methode transformiert übergebenen Punkt (= Pixel auf Screen)
-    //ruft isElemOfMand auf und gibt eine Farbe zurück
+    /**
+     * Methode transformiert übergebenen Punkt (= Pixel auf Screen)
+     * ruft isElemOfMand auf und gibt eine Farbe zurück
+     * @param i Punkt(i, j) im Koordinatensystem
+     * @param j
+     * @return Farbe für Pixel
+     */
     public int setColor(int i, int j) {
 
         //transalte und scale und mirror: a=i, b=j a' = i*sx * x - tx & b' = j*sy* y - ty
 
         //Int i, j als Complexe Zahl im Verhältnis zur Skalierung des Screens
-        Complex c = new Complex((double) i / this.width, (double) j / this.heigth);
+        Complex c = new Complex((double) i / this.width, (double) j / this.height);
         //scale
         c = c.scale(scale.getReal(), scale.getImag());
 
@@ -126,28 +131,51 @@ public class Algorithm {
         c = c.translate(-translate.getReal(), -translate.getImag());
 
         //überprüfen ob Punkt in Menge liegt und dem entsprechend Farbe zurückgeben
-        return isElemOfMand(c, maxIteration);
+        return isElemOfMand(c);
     }
 
 
 
+    /**
+     * Methode um Pixelarray mit Farbwerten befüllen
+     * @param granulation Auflösung der aktuellen Bitmap
+     * @return Pixel-Array, speichert Farben für Bitmap
+     */
+    public int[] fillPixels(int granulation){
+        int[] pixels = new int[width*height];
+        int loc;
+        int c;
+        //Farbpixel für jeweilige Auflösung berechnen
+        for (int i = 0; i < width - 1; i += granulation) {
+            for (int j = 0; j < height - granulation; j += granulation) {
+                //Koordinaten von 2D-Array zu 1D-Array umrechnen
+                loc = i + j * width;
+                //Farbe für Pixelblock berechnen
+                c = setColor(i, j);
+                pixels[loc] = c;
+                int n = granulation * granulation - 1;
 
-    void fillColor() {
-        System.out.println("fillColor()");
-        for (int i = 0; i < maxIteration; i++) {
-            if (i < maxIteration / 4) {
-                palette[i] = interpolate(color1, color2, i);
-            } else if (i < maxIteration / 2) {
-                palette[i] = interpolate(color2, color3, i);
-            } else if (i < maxIteration * 3 / 4) {
-                palette[i] = interpolate(color3, color4, i);
-            } else {
-                palette[i] = interpolate(color3, color4, i);
+                //Pixelblock in jeweiliger Granulation ins Array schreiben
+                while (n > 0) {
+                    if (n % granulation == 0) {
+                        loc = loc + width;
+                        pixels[loc] = c;
+                    }
+                    pixels[loc + (n % granulation)] = c;
+                    n--;
+                }
             }
-
         }
+        return pixels;
     }
 
+    /**
+     * Methode um zwischen Farben interpolieren
+     * @param v1 erste Farbe
+     * @param v2 zweite Farbe
+     * @param i index von Farb-Array
+     * @return interpolierte Farbe
+     */
     public int interpolate(int v1, int v2, int i) {
         int r = Math.abs(Color.red(v1) + i * (Color.red(v2) - Color.red(v1)) / maxIteration);
         int g = Math.abs(Color.green(v1) + i * (Color.green(v2) - Color.green(v1)) / maxIteration);
