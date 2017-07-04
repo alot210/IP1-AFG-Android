306b92407b8d28de71c40473b42122cca84535de
Thomas <schloemer.thomas@gmail.com>
2017-07-04 12:56:02 +0200

    Screenshot for >=Android 6

--- app/src/main/java/de/hsd/manguli/fractalsapp/MainActivity.java
+++ app/src/main/java/de/hsd/manguli/fractalsapp/MainActivity.java
@@ -1,19 +1,24 @@
 package de.hsd.manguli.fractalsapp;
 
+import android.Manifest;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -26,10 +31,6 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.tarek360.instacapture.Instacapture;
-import com.tarek360.instacapture.listener.SimpleScreenCapturingListener;
-import com.veer.hiddenshot.HiddenShot;
-
 import org.w3c.dom.Attr;
 
 import java.io.ByteArrayOutputStream;
@@ -41,7 +42,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
 
-import github.nisrulz.screenshott.ScreenShott;
+
 
 /**
  * MainActivity - wird beim Start der App aufgerufen
@@ -73,40 +74,49 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-
     }
 
-@Override
-public void onResume(){
-    super.onResume();
-    //Screenshot Button als FAB (=Floating Action Button) integrieren
-    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-    //Snackbar Meldung bei Klick auf FAB
-    fab.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Snackbar.make(view, "Dein Fractal wurde gespeichert.", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show();
+    @Override
+    public void onResume(){
+        super.onResume();
+        //Screenshot Button als FAB (=Floating Action Button) integrieren
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        //Snackbar Meldung bei Klick auf FAB
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Dein Fractal wurde gespeichert.", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
 
-            View myView = findViewById(R.id.view);
-            myView.buildDrawingCache();
-            Bitmap b1 = myView.getDrawingCache();
-            // copy this bitmap otherwise distroying the cache will destroy
-            // the bitmap for the referencing drawable and you'll not
-            // get the captured view
-            Bitmap b = b1.copy(Bitmap.Config.ARGB_8888, false);
-            BitmapDrawable d = new BitmapDrawable(b);
-            //canvasView.setBackgroundDrawable(d);
-            myView.destroyDrawingCache();
-            SaveImage(b);
-            // RootView
-            //Bitmap bitmap_rootview = ScreenShott.getInstance().takeScreenShotOfRootView(view);
-            //ScreenShott.getInstance().saveScreenshotToPicturesFolder(MainActivity.this, bitmap_rootview, "my_screenshot_filename");
-        }
-    });
-}
-    private void SaveImage(Bitmap finalBitmap) {
+                View myView = findViewById(R.id.view);
+                myView.buildDrawingCache();
+                Bitmap b1 = myView.getDrawingCache();
+                // copy this bitmap otherwise distroying the cache will destroy
+                // the bitmap for the referencing drawable and you'll not
+                // get the captured view
+                Bitmap b = b1.copy(Bitmap.Config.ARGB_8888, false);
+                //BitmapDrawable d = new BitmapDrawable(b);
+                //canvasView.setBackgroundDrawable(d);
+                myView.destroyDrawingCache();
+                if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            1);
+
+                    saveImage(b);
+
+                }
+                else {
+                    saveImage(b);
+                }
+                //sendBroadcast(new Intent(Intent.ACTION_MEDIA_MOUNTED, Uri.parse("file://"+ Environment.getExternalStorageDirectory())));
+                // RootView
+                //Bitmap bitmap_rootview = ScreenShott.getInstance().takeScreenShotOfRootView(view);
+                //ScreenShott.getInstance().saveScreenshotToPicturesFolder(MainActivity.this, bitmap_rootview, "my_screenshot_filename");
+            }
+        });
+    }
+    private void saveImage(Bitmap finalBitmap) {
 
         String root = Environment.getExternalStorageDirectory().toString();
         File myDir = new File(root + "/Pictures");
@@ -120,6 +130,7 @@ public void onResume(){
         try {
             FileOutputStream out = new FileOutputStream(file);
             finalBitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            notifyMediaStoreScanner(file);
             out.flush();
             out.close();
 
@@ -127,6 +138,7 @@ public void onResume(){
             e.printStackTrace();
         }
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -163,5 +175,50 @@ public void onResume(){
         return super.onOptionsItemSelected(item);
     }
 
+    public final void notifyMediaStoreScanner(final File file) {
+        try {
+            MediaStore.Images.Media.insertImage(this.getContentResolver(),
+                    file.getAbsolutePath(), file.getName(), null);
+            this.sendBroadcast(new Intent(
+                    Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(file)));
+            Log.d(TAG,"Hallo");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    View myView = findViewById(R.id.view);
+                    myView.buildDrawingCache();
+                    Bitmap b1 = myView.getDrawingCache();
+                    // copy this bitmap otherwise distroying the cache will destroy
+                    // the bitmap for the referencing drawable and you'll not
+                    // get the captured view
+                    Bitmap b = b1.copy(Bitmap.Config.ARGB_8888, false);
+                    //BitmapDrawable d = new BitmapDrawable(b);
+                    //canvasView.setBackgroundDrawable(d);
+                    myView.destroyDrawingCache();
+                    saveImage(b);
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(MainActivity.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
 }
