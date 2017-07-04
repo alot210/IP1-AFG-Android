8652262550a76a6cecae50c2ee7ecc150870cf6b
chris-95 <post0095@gmail.com>
2017-07-04 17:36:47 +0200

    added zoom

--- app/src/main/java/de/hsd/manguli/fractalsapp/views/FractalView.java
+++ app/src/main/java/de/hsd/manguli/fractalsapp/views/FractalView.java
@@ -13,6 +13,8 @@ import android.os.Build;
 import android.text.format.DateUtils;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 
 import com.flask.colorpicker.ColorPickerView;
@@ -44,6 +46,39 @@ public class FractalView extends View {
     Boolean juliaPush = JuliaFragment.juliaPush;
     Boolean mandelPush = MandelbrotFragment.mandelPush;
     Algorithm am;
+
+    /**
+     * statische Variablen für die minimale und maximale Zoom-Frequenz
+     */
+    private static float MIN_ZOOM = 1f;
+    private static float MAX_ZOOM = 5f;
+
+    //Skalierungsfaktor
+    private float scaleFactor = 1.f;
+
+    //erstellen eines ScaleGestureDetectors
+    private ScaleGestureDetector gestureDetector;
+
+    //wenn kein Finger den Bildschirm berührt
+    private static int NONE = 0;
+    //wenn 1 Finger den Bildschirm berührt
+    private static int DRAG = 1;
+    //wenn 2 Finger den Bildschirm berühren
+    private static int ZOOM = 2;
+
+    private int mode;
+
+    //x- und y-Koordinaten des ersten Fingers der gesetzt wird
+    private float startX = 0f;
+    private float startY = 0f;
+
+    //x- und y-Koordinaten des zweiten Fingers der gesetzt wird
+    private float translateX = 0f;
+    private float translateY = 0f;
+
+    //Koordinaten geben an wo zuletzt hingedragged wurde
+    private float previousTranslateX = 0f;
+    private float previousTranslateY = 0f;
 
     //Auflösung wird mit 16x16, 8x8, 4x4 und 2x2 berechnet
     private int granulation = 16;
@@ -86,16 +121,19 @@ public class FractalView extends View {
     //Überschreiben der drei Constructor
     public FractalView(Context context) {
         super(context);
+        gestureDetector = new ScaleGestureDetector(context,new ScaleListener());
         init();
     }
 
     public FractalView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        gestureDetector = new ScaleGestureDetector(context,new ScaleListener());
         init();
     }
 
     public FractalView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        gestureDetector = new ScaleGestureDetector(context,new ScaleListener());
         init();
     }
 
@@ -132,7 +170,13 @@ public class FractalView extends View {
             return;
         }
         if (bitmap != null) {
+            canvas.save();
+            canvas.scale(this.scaleFactor,this.scaleFactor,this.gestureDetector.getFocusX(),this.gestureDetector.getFocusY());
+            //translate soll nicht außerhalb des Canvas stattfinden
+            scaleWindow(canvas);
+            canvas.translate(translateX/scaleFactor,translateY/scaleFactor);
             canvas.drawBitmap(bitmap, 0, 0, paint);
+            canvas.restore();
             Log.d("LOGGING", "drawBitmap()");
             return;
         }
@@ -271,4 +315,89 @@ public class FractalView extends View {
                 });
         return cpdb;
     }//end getColorPickerDialogBuilder
+
+    /**
+     * handelt das Touch Event
+     * @param event Touch Event dass beim Berühren des Screens übergeben wird
+     * @return
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+
+        switch (event.getAction() & MotionEvent.ACTION_MASK){
+            case MotionEvent.ACTION_DOWN:
+                mode = DRAG;
+                //die aktuellen Koordinaten des Fingers beim Berühren des Screens
+                startX = event.getX()- previousTranslateX;
+                startY = event.getY()- previousTranslateY;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                //wird bei jeder Bewegung des zweiten Fingers geupdatet
+                translateX = event.getX() - startX;
+                translateY = event.getY() - startY;
+                break;
+            case MotionEvent.ACTION_POINTER_DOWN:
+                //Ist der zweite Finger gesetzt kann gezoomt werden
+                mode = ZOOM;
+                break;
+            case MotionEvent.ACTION_UP:
+                mode = NONE;
+                //hier wird gespeichert wo zuletzt hingedragged wurde
+                previousTranslateX = translateX;
+                previousTranslateY = translateY;
+                break;
+            case MotionEvent.ACTION_POINTER_UP:
+                mode = DRAG;
+                //hier wird gespeichert wo zuletzt hingezoomt wurde
+                previousTranslateX = translateX;
+                previousTranslateY = translateY;
+                break;
+        }
+
+        gestureDetector.onTouchEvent(event);
+        //wird neu gezeichnet, wenn der Faktor größer als 1f ist oder der Modus ZOOM erkannt wurde
+        if((mode == DRAG && scaleFactor != 1f) || mode == ZOOM){
+            invalidate();
+        }
+        return true;
+    }//end onTouchEvent
+
+    /**
+     * Methode überprüft, dass nicht aus dem Canvas herausgezoomt werden kann
+     * @param canvas das übergebene Canvas-Objekt auf das gezeichnet wird
+     */
+    public void scaleWindow(Canvas canvas){
+        if((translateX*-1)< 0){
+            translateX = 0;
+        }
+        else if((translateX*-1)> (scaleFactor -1)* canvas.getWidth()){
+            translateX = (1- scaleFactor)* canvas.getWidth();
+        }
+
+        if((translateY*-1)< 0){
+            translateY = 0;
+        }
+        else if((translateY*-1)> (scaleFactor -1)* canvas.getHeight()){
+            translateY = (1- scaleFactor)* canvas.getHeight();
+        }
+    }//end scaleWindow
+
+    /**
+     * Klasse wird im Konstruktor von FractalView aufgerufen
+     */
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener{
+        /**
+         * aktueller Skalierungsfaktor wird hier übergeben
+         * es wird überprüft ob dieser innerhalb unseres ausgewählten Fensters
+         * liegt, wenn ja wird der Zoom ausgeführt
+         * @param gestureDetector
+         * @return
+         */
+        public boolean onScale(ScaleGestureDetector gestureDetector){
+            scaleFactor *= gestureDetector.getScaleFactor();
+            scaleFactor = Math.max(MIN_ZOOM, Math.min(scaleFactor, MAX_ZOOM));
+
+            return true;
+        }//end onScale
+    }//end class ScaleListener()
 }//end class()
